f049f6e01acbacae8d9e5769937f5d8a586a80ce
Louis Dang <louisd@Louiss-MacBook-Pro.local>
2024-12-21 03:25:36 +0700

    feat: remove some data call

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { api } from '@/convex/_generated/api';
-import { preloadQuery } from 'convex/nextjs';
 import DashboardHomePage from './dashboard';
-import { getAuthToken } from '../auth';
 
 const ServerDashboardHomePage = async () => {
-  const token = await getAuthToken();
+  // const token = await getAuthToken();
   // const preloadedNotes = await preloadQuery(api.notes.getNotes, {}, { token });
 
   return <DashboardHomePage />;
